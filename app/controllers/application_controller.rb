b8dea4614b39968a5bd2208590ecb44aa0fc8933
Mike Donovan <creativemood@ymail.com>
2012-10-09 23:20:13 +0200

    fix redirect path according to new routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    profile_path(id: resource.profile_name)
+  end
 end
