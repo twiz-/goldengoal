5a2234354632cfa5be1b5bccf679c87b9ced0886
Mike Donovan <creativemood@ymail.com>
2012-10-09 23:20:44 +0200

    Implementing abstract private controller

---
+++ app/controllers/private_controller.rb
@@ -0,0 +1,18 @@
+class PrivateController < ApplicationController
+  before_filter :authenticate_user! 
+  before_filter :find_user
+  before_filter :guard_user
+
+  protected 
+  
+  def find_user
+    @user = User.find_by_profile_name!(params[:id])
+  end
+
+  def guard_user
+    if @user != current_user
+      raise ActiveRecord::RecordNotFound 
+    end
+  end
+
+end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,8 @@
-class ProfilesController < ApplicationController
+class ProfilesController < PrivateController
+
   def show
-    @user = User.find_by_profile_name(params[:id])
-    if @user
-      @players = @user.players.all
-      render action: :show
-    else
-      render file: 'public/404', status: 404, formats: [:html]
-    end
+    @players = @user.players.all
+    render action: :show
   end
+
 end
